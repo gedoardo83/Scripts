28804b64dba26b91703dcb6175cc2b7d3f3b4fc8
gedoardo83 <edoardo.giacopuzzi@unibs.it>
2019-03-19 12:54:55 +0100

    Perl modules with my useful functions

---
+++ Perl_functions.pm
@@ -0,0 +1,37 @@
+############################################################################
+###  Read VCF file and return 2 hashes containing INFO tags and GT tags  ###
+############################################################################
+sub read_VCF {
+	my ($inputfile) = @_;
+	open(IN, $inputfile);
+	while ($row = <IN>) {
+		chomp($row);
+		if ($row =~ /^##/) {next}
+		if ($row =~ /^#CHROM/) {
+			@line = split("\t", $row);
+			@samplesid=@line[9..$#line];
+			next;
+		}
+		@line = split("\t", $row);
+		
+		#read info tags and their values and store in infos hash
+		@infotags = split(";", $line[7]);
+		foreach (@infotags) {
+			$_ =~ /(.+)=(.+)/;
+			$infos{$1}=$2;
+		}
+
+		#read format tags and values for each sample and store in gtinfo hash
+		@format=split(":", $line[8]);
+		$i=9;
+		foreach $mysample(@samplesid) {
+			@column=split(":", $line[$i]);
+			for ($n=0; $n<=$#format; $n++) {
+		    	$gtinfo{$mysample}{$format[$n]}=$column[$n];
+			}
+		$i++;
+		}
+	}
+	close(IN);
+	return (\%infos, \%gtinfo);
+}
