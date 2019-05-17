166b017d1b77f20e6fdf5ea56cbf72c6e9227320
gedoardo83 <edoardo.giacopuzzi@unibs.it>
2019-05-17 12:06:17 +0200

    Update Convert_gene_ids_biomaRt.R

--- Convert_gene_ids_biomaRt.R
+++ Convert_gene_ids_biomaRt.R
@@ -1,3 +1,3 @@
-library(bioMart)
+library(biomaRt)
 ensembl <- useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", GRCh=37)
-gene_ensembl <- getBM(attributes=c('ensembl_gene_id','hgnc_symbol'), filters='hgnc_symbol', values=[list ids], mart=ensembl
+gene_ensembl <- getBM(attributes=c('ensembl_gene_id','hgnc_symbol'), filters='hgnc_symbol', values=[list ids], mart=ensembl)
